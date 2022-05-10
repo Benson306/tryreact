e991984f1658f6033af3d3107177ad6351f17117
Benson Ndiwa <bnkimtai@gmail.com>
2022-05-10 19:08:42 +0300

    Adding components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar  from './Navbar';
+import Home from './Home';
 
 function App() {
+//   const title = 'Benji is Awesome';
+//   const likes = 500;
+  // const link = "https://kimtech-real-estate.herokuapp.com";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="content">  
+        <Home />
+        {/* <h1>{ title }</h1>
+        <p>Liked: { likes } times</p>
+        <p>{10 + 5}</p>
+        <p>{"Hey Ninjas"}</p>
+        <p>{ Math.random() +10 }</p>
+        <a href={link}>My App</a> */}
+      </div>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => { //sfc
+    return (  
+        <nav className="navbar">
+            <h1>The Benji Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
