f1c2eaee9437ecb624059176581a314c3ff7e7db
Benson Ndiwa <bnkimtai@gmail.com>
2022-05-19 19:48:54 +0300

    Props

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
     // let sname = 'benji';
@@ -38,16 +39,8 @@ const Home = () => {
             <br />
             <br /> */}
 
-
-            {
-            blogs.map((blog)=>(
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by: {blog.author}</p>
-                    <br />
-                </div>   
-            ))
-            }
+            <BlogList blogs={blogs} title='All Blogs!'/>
+            
 
 
         </div>
