5e5bded49023bb38de0605add99a84b647dde419
Benson Ndiwa <bnkimtai@gmail.com>
2022-05-19 19:14:14 +0300

    Dev tools

--- src/Home.js
+++ src/Home.js
@@ -4,26 +4,31 @@ const Home = () => {
     // const handleClick = function(e){
     //     console.log('hello ninjas',e);
     // }
+    // const handleClickAgain = (name)=>{
+    //     console.log("hello "+name )
+    // }
+
     const [sname, setName] = useState('mario')
+    const [age, setAge] = useState(7);
     
     const dispName = ()=>{
         setName('luigi');
+        setAge(24)
     }
 
-    const handleClickAgain = (name)=>{
-        console.log("hello "+name )
-    }
+    
     return ( 
         <div className="home">
             <h2>HomePage</h2>
 
-            <p>{sname}</p>
+            <p>Name: {sname}</p>
+            <p>Age: {age}</p>
             <br />
             <button onClick={dispName}>Change Name</button>
             {/* <button onClick={handleClick}>Click me</button> */}
             <br />
             <br />
-            <button onClick={()=> handleClickAgain('mario')}>Click me again</button>
+            {/* <button onClick={()=> handleClickAgain('mario')}>Click me again</button> */}
             <br />
             <br />
         </div>
