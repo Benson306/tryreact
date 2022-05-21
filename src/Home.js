ccfa7c9d0e7133a688634fde4ac50dfa2c2d38a6
Benson Ndiwa <bnkimtai@gmail.com>
2022-05-21 12:42:13 +0300

    fetching JSON data

--- src/Home.js
+++ src/Home.js
@@ -17,22 +17,37 @@ const Home = () => {
     //     setName('luigi');
     //     setAge(24)
     // }
-    const [blogs, setBlogs] = useState([
-        {title: 'My new website', body: 'lorem ipsum ....', author: 'mario', id: 1},
-        {title: 'Welcome party!', body: 'lorem ipsum ....', author: 'yoshi', id: 2},
-        {title: 'Web dev top tips', body: 'lorem ipsum ....', author: 'mario', id: 3}
-    ]);
+    // const [blogs, setBlogs] = useState([
+    //     {title: 'My new website', body: 'lorem ipsum ....', author: 'mario', id: 1},
+    //     {title: 'Welcome party!', body: 'lorem ipsum ....', author: 'yoshi', id: 2},
+    //     {title: 'Web dev top tips', body: 'lorem ipsum ....', author: 'mario', id: 3}
+    // ]);
+
+    // const handleDelete = (id)=>{
+    //     const newBlogs = blogs.filter((blog)=> blog.id!== id)
+    //     setBlogs(newBlogs)
+    // }
+
+    const [blogs, setBlogs] = useState(null);
 
     const [name, setName] = useState('Benji');
 
-    const handleDelete = (id)=>{
-        const newBlogs = blogs.filter((blog)=> blog.id!== id)
-        setBlogs(newBlogs)
-    }
+   
+
+    // useEffect(()=>{
+    //     console.log(name);
+    // },[name]);
 
     useEffect(()=>{
-        console.log(name);
-    },[name]);
+        fetch('http://localhost:8000/blogs')
+        .then( res=>{
+            return res.json();
+        })
+        .then((data)=>{
+            console.log(data)
+            setBlogs(data)
+        })
+    }, []);
 
     
     return ( 
@@ -51,8 +66,10 @@ const Home = () => {
             <BlogList blogs={blogs.filter( (blog)=> blog.author === 'mario')} title="Mario's Blogs!"/>
             */}
 
-            <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete}/>
-<br />
+            {/* {blogs && <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete}/> } */}
+
+            {blogs && <BlogList blogs={blogs} title='All Blogs!'/>} 
+            <br />
             <button onClick={()=>setName('Luigi')}>Change Name</button>
             <p>{name}</p>
             
