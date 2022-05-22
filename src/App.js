359f1edeb25a4a782683f58d311d35aa3b64bbac
Benson Ndiwa <bnkimtai@gmail.com>
2022-05-22 16:55:25 +0300

    404 pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './Footer'
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
 //   const title = 'Benji is Awesome';
@@ -27,6 +28,10 @@ function App() {
 
           <Route path="/blogs/:id">
             <BlogDetails />
+          </Route>
+
+          <Route path="*">
+            <NotFound />
           </Route>
           
         </Switch>
